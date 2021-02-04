8d3fdf19ff3828bc81d8ce95c374f9f128953ba2
rajanimohan22 <mohan.dolla@digital.hmrc.gov.uk>
2021-02-04 15:50:43 +0000

    LicenseYear updates

--- app/uk/gov/hmrc/helptosavereminder/actors/ProcessingSupervisor.scala
+++ app/uk/gov/hmrc/helptosavereminder/actors/ProcessingSupervisor.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,7 +97,8 @@ class ProcessingSupervisor @Inject()(
 
       (isUserScheduleEnabled, isExpressionValid) match {
         case (true, true) =>
-          Logger.info(s"[ProcessingSupervisor] BOOTSTRAP is scheduled with userScheduleCronExpression = $userScheduleCronExpression")
+          Logger.info(
+            s"[ProcessingSupervisor] BOOTSTRAP is scheduled with userScheduleCronExpression = $userScheduleCronExpression")
           scheduler
             .createSchedule(
               "UserScheduleJob",
